6b6b268f9d1e5d20af446aecb01bee4965ecccfb
lxz <liuxianzhie@qq.com>
2020-08-04 11:32:30 +0800

    integrate spring security

---
+++ security/src/main/java/com/fire/security/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.fire.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String testHello() {
+        return "hello";
+    }
+}
--- security/src/test/java/com/fire/security/SecurityApplicationTests.java
+++ security/src/test/java/com/fire/security/SecurityApplicationTests.java
@@ -1,6 +1,6 @@
 package com.fire.security;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
